b50dbe942b1eba8498eb9bfe8ec9e00a40efa06a
venkataswami <venkataswami@192.168.1.200>
2019-08-05 03:20:38 -0700

    new organic search code_2 links

---
+++ test_buildeeji/src/testing/update_organic_search_code.java
@@ -0,0 +1,90 @@
+package testing;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+
+public class update_organic_search_code {
+
+	public void tc() throws IOException, InterruptedException {
+
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://www.google.com/");
+
+		ArrayList<String> URLs = Readexceldata(0);
+
+		System.out.println("List    : : : " + URLs.size());
+
+		for (int i = 0; i < URLs.size(); i++) {
+
+			driver.findElement(By.className("gLFyf")).sendKeys(URLs.get(i));
+
+			driver.findElement(By.className("gLFyf")).sendKeys(Keys.ENTER);
+
+			System.out.println(URLs.get(i));
+
+			for (int k = 1; k <= 2; k++) {
+
+				driver.findElement(By.xpath(".//*[@id='rso']/div[1]/div/div[" + k + "]/div/div/div[1]/a/div/cite")).click();
+
+				Thread.sleep(1000);
+				driver.navigate().back();
+			}
+
+			Thread.sleep(3000);
+			driver.navigate().back();
+			Thread.sleep(1000);
+			driver.findElement(By.className("gLFyf")).clear();
+
+		}
+		System.out.println("Task completed");
+	}
+
+	public ArrayList<String> Readexceldata(int rowNo) throws IOException {
+
+		FileInputStream fis = new FileInputStream("C:\\Users\\venkataswami\\Desktop\\data1212454545.xls");
+
+		HSSFWorkbook wb = new HSSFWorkbook(fis);
+
+		HSSFSheet s = wb.getSheet("first place");
+
+		Iterator<Row> rowiterator = s.iterator();
+
+		rowiterator.next();
+
+		ArrayList<String> list = new ArrayList<String>();
+		while (rowiterator.hasNext()) {
+			list.add(rowiterator.next().getCell(rowNo).getStringCellValue());
+
+		}
+
+		return list;
+
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		update_organic_search_code data = new update_organic_search_code();
+
+		data.tc();
+	}
+
+}
